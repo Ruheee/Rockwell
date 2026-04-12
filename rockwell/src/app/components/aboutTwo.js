0166149847ce46d28c1b976b9a2d98b25dafb677
Rui Dantas <ruidantas55@gmail.com>
2026-04-12 15:01:08 -0400

    vercel

--- rockwell/src/app/components/aboutTwo.js
+++ rockwell/src/app/components/aboutTwo.js
@@ -18,7 +18,7 @@ export default function AboutBottom() {
 
       </div>
       <img
-          src="/construction.png"
+          src="/Construction.png"
           alt="Construction site"
           className={styles.imageEdit}
           
